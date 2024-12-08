902f55c807a5cfee994beab9fd4f8d5b8631ac92
Sohaib <sohaibshaukat78@gmail.com>
2024-12-08 14:12:28 +0500

    ads

--- react-frontend/src/components/ad.jsx
+++ react-frontend/src/components/ad.jsx
@@ -1,20 +1,4 @@
-import { useEffect, useState } from "react";
-
 const Ad = () => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const element = document.getElementById("ad-component");
-      const rect = element.getBoundingClientRect();
-      if (rect.top < window.innerHeight && rect.bottom >= 0) {
-        setIsVisible(true);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
 
   return (
     <div
@@ -22,9 +6,7 @@ const Ad = () => {
       className={`hidden md:flex flex-col w-full md:w-[25%] h-[90%] rounded-xl mt-[2.4rem] 
       bg-cover bg-center bg-no-repeat shadow-lg
       bg-[url('https://images.unsplash.com/photo-1695345272166-4efd76dd7a21?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]
-      transition-opacity duration-700 ease-out ${
-        isVisible ? "opacity-100" : "opacity-0"
-      }`}
+      transition-opacity duration-700 ease-out }`}
     >
       {/* Overlay for content */}
       <div className="flex flex-col items-center w-full h-full rounded-xl bg-black bg-opacity-50 p-4 md:p-6 text-center">
